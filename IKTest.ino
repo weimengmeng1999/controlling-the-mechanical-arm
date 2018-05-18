4170cf89d4627b65857d672f6410f669d484c92e
weimengmeng1999 <38174026+weimengmeng1999@users.noreply.github.com>
2018-05-18 17:39:34 +0800

    Add files via upload

---
+++ IKTest.ino
@@ -0,0 +1,30 @@
+
+#include "meArm.h"
+#include <Servo.h>
+
+int basePin = 11;
+int shoulderPin = 10;
+int elbowPin = 9;
+int gripperPin = 6;
+
+meArm arm;
+void setup() {
+  arm.begin(basePin, shoulderPin, elbowPin, gripperPin);
+  pinMode(7,INPUT);
+}
+
+void loop() {
+ 
+ 
+  delay(500);
+  arm.gotoPoint(-90,130,140);
+  arm.openGripper();
+  arm.closeGripper();
+  if(arm.gotoPoint(90,130,60)==1){
+    delay(1000);
+     arm.openGripper();
+    };
+  arm.gotoPoint(70,200,10);
+  arm.gotoPoint(0,100,50);
+  delay(1000);
+}
---
+++ meArm.h
@@ -0,0 +1,46 @@
+///
+#ifndef MEARM_H
+#define MEARM_H
+
+#include <Arduino.h>
+#include <Servo.h>
+
+const float pi=3.14159265359;
+
+struct ServoInfo {
+    int n_min, n_max;   // PWM 'soft' limits - should be just within range
+    float gain;         // PWM per radian
+    float zero;         // Theoretical PWM for zero angle
+};
+
+class meArm {
+  public:
+    //Full constructor uses calibration data, or can just give pins
+    meArm(int sweepMinBase=145, int sweepMaxBase=49, float angleMinBase=-pi/4, float angleMaxBase=pi/4,
+      int sweepMinShoulder=118, int sweepMaxShoulder=22, float angleMinShoulder=pi/4, float angleMaxShoulder=3*pi/4,
+      int sweepMinElbow=144, int sweepMaxElbow=36, float angleMinElbow=pi/4, float angleMaxElbow=-pi/4,
+      int sweepMinGripper=75, int sweepMaxGripper=115, float angleMinGripper=pi/2, float angleMaxGripper=0);
+    //required before running
+    void begin(int pinBase, int pinShoulder, int pinElbow, int pinGripper);
+    //Travel smoothly from current point to another point
+    int gotoPoint(float x, float y, float z);
+    //Set servos to reach a certain point directly without caring how we get there 
+    void goDirectlyTo(float x, float y, float z);
+    //Grab something
+    void openGripper();
+    //Let go of something
+    void closeGripper();
+    //Check to see if possible
+    bool isReachable(float x, float y, float z);
+    //Current x, y and z
+    float getX();
+    float getY();
+    float getZ();
+  private:
+    float _x, _y, _z;
+    Servo _base, _shoulder, _elbow, _gripper;
+    ServoInfo _svoBase, _svoShoulder, _svoElbow, _svoGripper;
+    int _pinBase, _pinShoulder, _pinElbow, _pinGripper;
+};
+
+#endif
